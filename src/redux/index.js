d63d4c1e4ffcb6562a2dcc7510a1af6e17f7cd27
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2022-09-25 01:41:46 +0300

    Add store for theme

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,11 +2,13 @@ import {configureStore} from '@reduxjs/toolkit';
 
 import todo from './todo';
 import modal from './modal';
+import theme from './theme';
 
 const store = configureStore({
   reducer: {
     todo,
     modal,
+    theme,
   },
 });
 
