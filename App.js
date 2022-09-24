8b63d5d79987ff382f3f38112a9e8d7818b1d923
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2022-09-25 01:38:57 +0300

    Add logic for theme

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, StatusBar} from 'react-native';
 import React from 'react';
 
 import colors from './src/theme/colors';
@@ -12,27 +12,58 @@ import ModalComponent from './src/components/Modal';
 import {Provider} from 'react-redux';
 import store from './src/redux';
 
-const App = () => {
+import {useSelector} from 'react-redux';
+
+const AppWrapper = () => {
   return (
     <Provider store={store}>
-      <SafeAreaView style={styles.container}>
-        <Header />
-        <TodoList />
-        <Button />
-
-        <ModalComponent />
-      </SafeAreaView>
+      <App />
     </Provider>
   );
 };
 
+/* 
+could not find react-redux context value; please ensure the component is wrapped in a <Provider>
+Bu hatayı veriyor.
+Önerilen yöntem ise Provider içerisini ayrı bir component yapmak.
+
+*/
+const App = () => {
+  const {isDark} = useSelector(state => state.theme);
+
+  return (
+    <>
+      <SafeAreaView
+        style={{
+          height: units.height / 20,
+          backgroundColor: isDark ? colors.BLACK : colors.WHITE,
+        }}>
+        <StatusBar
+          translucent
+          barStyle={isDark ? 'light-content' : 'dark-content'}
+        />
+      </SafeAreaView>
+      <SafeAreaView
+        style={isDark ? styles.darkContainer : styles.lightContainer}>
+        <Header isDark={isDark} />
+
+        <TodoList />
+        <Button isDark={isDark} />
+
+        <ModalComponent />
+      </SafeAreaView>
+    </>
+  );
+};
 const styles = StyleSheet.create({
-  container: {
+  darkContainer: {
+    flex: 1,
+    backgroundColor: colors.BLACK,
+  },
+  lightContainer: {
     flex: 1,
     backgroundColor: colors.WHITE,
-    marginTop: units.height / 15,
-    marginHorizontal: units.width / 10,
   },
 });
 
-export default App;
+export default AppWrapper;
