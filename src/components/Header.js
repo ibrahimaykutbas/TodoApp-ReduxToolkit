9456c0f5f0230945cab1921193d57d3e8df9f17f
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2022-09-25 01:40:23 +0300

    Add logic for theme to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,46 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
 import React from 'react';
 
 import units from '../theme/units';
 import fonts from '../theme/fonts';
 import colors from '../theme/colors';
 
-import Moon from '../assets/svgs/moon.svg';
-import Glass from '../assets/svgs/glass.svg';
+import DarkMoon from '../assets/svgs/dark-moon.svg';
+import LightMoon from '../assets/svgs/light-moon.svg';
+import DarkGlass from '../assets/svgs/dark-glass.svg';
+import LightGlass from '../assets/svgs/light-glass.svg';
 
-const Header = () => {
+import {useDispatch} from 'react-redux';
+
+import {changeTheme as changeThemeRedux} from '../redux/theme';
+
+const Header = ({isDark}) => {
+  const dispatch = useDispatch();
+
+  const changeTheme = () => {
+    dispatch(changeThemeRedux());
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.topContent}>
-        <Moon width={units.width / 15} height={units.width / 15} />
-        <Glass width={units.width / 15} height={units.width / 15} />
+        <Pressable onPress={changeTheme}>
+          {isDark ? (
+            <LightMoon width={units.width / 15} height={units.width / 15} />
+          ) : (
+            <DarkMoon width={units.width / 15} height={units.width / 15} />
+          )}
+        </Pressable>
+        {isDark ? (
+          <LightGlass width={units.width / 15} height={units.width / 15} />
+        ) : (
+          <DarkGlass width={units.width / 15} height={units.width / 15} />
+        )}
       </View>
       <View style={styles.bottomContent}>
-        <Text style={styles.today}>TODAY</Text>
+        <Text style={[styles.today, isDark && {color: colors.WHITE}]}>
+          TODAY
+        </Text>
         <Text style={styles.tomorrow}>TOMORROW</Text>
       </View>
     </View>
@@ -27,6 +51,8 @@ export default Header;
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: units.height / 40,
+    marginHorizontal: units.width / 10,
     marginBottom: units.height / 25,
   },
   topContent: {
